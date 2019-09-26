3bc6c9577253ae97897f7a224658429c28cfaa99
sanjeet kumar <37035031+sanjeetma@users.noreply.github.com>
2019-09-26 22:47:28 +0530

    Add files via upload

--- Util.java
+++ Util.java
@@ -212,26 +212,6 @@ public class Util {
 		return num2;
 		
 	}
-	public static int dayOfWeek(int month,int day,int year) {
-		int y=year;
-		int m=month;
-		int d=day;
-		int y0=y-(14-m)/12;
-		int x=y0+(y0/4)-(y0/100)+(y0/400);
-		int m0=(m+12*((14-m)/12))-2;
-		int d0=(d+x+(31*m0)/12)%7;
-				
-	   return d0;
-	}
-	public static double monthlyPayment(double Y,double P,double R) {
-		double payment;
-		double n=12*Y;
-		double r=R/(12*100);
-		double temp=Math.pow(1+r, -n);
-		payment=(P*r)/1-temp;
-		return payment;
-		
-	}
 }
    
 	
