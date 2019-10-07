76fda4b34928b31d43c5104d3959d0d5f1e73c8e
sanjeet kumar <37035031+sanjeetma@users.noreply.github.com>
2019-10-08 02:40:56 +0530

    working on Clinique management

---
+++ java/bridgelab/OopS/AddressMain.java
@@ -0,0 +1,15 @@
+package com.java.bridgelab.oops;
+
+import java.io.IOException;
+
+import org.json.simple.parser.ParseException;
+
+public class AddressMain {
+
+	public static void main(String[] args) throws IOException, ParseException {
+		AddressBook obj=new AddressBook();
+		obj.choose();
+
+	}
+
+}
